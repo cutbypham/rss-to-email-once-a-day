e323704b20811718f5868fc34a202bcb476a3bf2
cutbypham <cutbypham@gmail.com>
2025-03-13 11:27:06 +0700

    👌Auto commit

--- src/feeds.ts
+++ src/feeds.ts
@@ -4,4 +4,8 @@ export const feeds = [
   'https://www.youtube.com/feeds/videos.xml?channel_id=UC_y92TyYNuw-QVk10i5IOAA',
   // 7789vlog
   'https://www.youtube.com/feeds/videos.xml?channel_id=UClK96yyeo-Hg3lZ86vrqVjQ',
+  // THỎ BẢY MÀU
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCqb-IU-JKgCg2aqZw3gJafQ',
+  // Natalie Lynn
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCkjrBN_GAjFJyVvjcI07KkQ',
 ]
