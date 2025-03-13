4b6d3ad1ca5b1b21dc9235023e3371cde9aea0d3
cutbypham <cutbypham@gmail.com>
2025-03-13 11:26:07 +0700

    👌Auto commit

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,3 +1,7 @@
 export const feeds = [
   'https://geohot.github.io/blog/feed.xml',
+  // viet riders
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC_y92TyYNuw-QVk10i5IOAA',
+  // 7789vlog
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UClK96yyeo-Hg3lZ86vrqVjQ',
 ]
