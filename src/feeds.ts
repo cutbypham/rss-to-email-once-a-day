f05cd04f8041fa688c67f56152a5a34f7900f802
cutbypham <cutbypham@gmail.com>
2025-06-06 15:23:51 +0700

    👌Auto commit

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,39 +1,3 @@
 export const feeds = [
   'https://geohot.github.io/blog/feed.xml',
-  // viet riders
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UC_y92TyYNuw-QVk10i5IOAA',
-  // 7789vlog
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UClK96yyeo-Hg3lZ86vrqVjQ',
-  // THỎ BẢY MÀU
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCqb-IU-JKgCg2aqZw3gJafQ',
-  // Natalie Lynn
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCkjrBN_GAjFJyVvjcI07KkQ',
-  // Di Hay Đi
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UChQqVI5-nWM9tmP5IryWEWQ',
-  // Jimmy Kim
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCOU2PEQuXiz4JsfEtW3frhA',
-  // Creative Video Tips | davinci resolve
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UC6TDwXA0UhifKuBzqLZAqHg',
-  //MrAlexTech
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCTmY-LBmbLugkJ0ryZfjZTQ',
-  //Chad Kubanoff
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCo4K5kzinPI9AHRDp_V4T0w',
-  // Long Huỳnh
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCc8ujKhuBXclWlm49yd7Plw',
-  // THỎ BẢY MÀU
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCqb-IU-JKgCg2aqZw3gJafQ',
-  // Tricky Prolix, video editor
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCdroOgO1Hn90pqBrTvsuX4A',
-  // Wampus, video editor
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCrTufzp1J315kdnf_nzy6iQ',
-  // FutureCanoe
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCF3jMqkgvFRb6KBbSfsFFtQ',
-  // Đấy Chấm Than
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UClMkagB7TmoXeFJ_9YOv-Rw',
-  // Probably Riding
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCQEDB9BVgbMKF8gaOW75WGQ',
-  // Minh Cycling Lee
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCYSVPcOCjENxwy5j5ew0hUg',
-  // Digital Spaghetti
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCTdsYrCpF2zRKWWL0SkaOtA',
 ]
