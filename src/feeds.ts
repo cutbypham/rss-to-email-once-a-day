eee395f3254d33b6827aa306811d023f0235d7bc
cutbypham <cutbypham@gmail.com>
2025-06-29 14:00:22 +0700

    Auto commit

--- src/feeds.ts
+++ src/feeds.ts
@@ -3,4 +3,10 @@ export const feeds = [
     ////////// video editing //////////
     // Wampus
     'https://www.youtube.com/feeds/videos.xml?channel_id=UCrTufzp1J315kdnf_nzy6iQ',
+    // Cullen Kelly
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCHZD22Eei2KX8wpJUzk0Bxg',
+    //Creative Video Tips
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UC6TDwXA0UhifKuBzqLZAqHg',
+    // Darren Mostyn
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UC6LqojU1x4LWWhL7QqLB8Mw',
 ]
