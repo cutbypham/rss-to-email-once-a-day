b17ed853ce8dc1c83efeecf729b972742d459609
cutbypham <cutbypham@gmail.com>
2025-06-29 13:06:26 +0700

    👌Auto commit

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,3 +1,6 @@
 export const feeds = [
-  'https://geohot.github.io/blog/feed.xml',
+    'https://geohot.github.io/blog/feed.xml',
+    ////////// video editing //////////
+    // Wampus
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCrTufzp1J315kdnf_nzy6iQ',
 ]
