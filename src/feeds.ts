468074c1e6898e600cd5e67c9a8145550013c0a5
cutbypham <cutbypham@gmail.com>
2025-07-01 13:00:19 +0700

    Auto commit

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,39 +1,4 @@
 export const feeds = [
     'https://geohot.github.io/blog/feed.xml',
     ////////// video editing //////////
-    // Wampus
-    'https://www.youtube.com/feeds/videos.xml?channel_id=UCrTufzp1J315kdnf_nzy6iQ',
-    // Cullen Kelly
-    'https://www.youtube.com/feeds/videos.xml?channel_id=UCHZD22Eei2KX8wpJUzk0Bxg',
-    //Creative Video Tips
-    'https://www.youtube.com/feeds/videos.xml?channel_id=UC6TDwXA0UhifKuBzqLZAqHg',
-  //MrAlexTech
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCTmY-LBmbLugkJ0ryZfjZTQ',
-    // Darren Mostyn
-    'https://www.youtube.com/feeds/videos.xml?channel_id=UC6LqojU1x4LWWhL7QqLB8Mw',
-    
-   ////////// cycling //////////
-   // viet riders
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UC_y92TyYNuw-QVk10i5IOAA',
-  // Di Hay Đi
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UChQqVI5-nWM9tmP5IryWEWQ',
-  // Long Huỳnh
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCc8ujKhuBXclWlm49yd7Plw',
-
-   ////////// food //////////
-  //Chad Kubanoff
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCo4K5kzinPI9AHRDp_V4T0w',
-  // FutureCanoe
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCF3jMqkgvFRb6KBbSfsFFtQ',
-
-  // Natalie Lynn
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCkjrBN_GAjFJyVvjcI07KkQ',
-  // 7789vlog
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UClK96yyeo-Hg3lZ86vrqVjQ',
-  // Đấy Chấm Than
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UClMkagB7TmoXeFJ_9YOv-Rw',
-
-  // THỎ BẢY MÀU
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCqb-IU-JKgCg2aqZw3gJafQ',
-
 ]
