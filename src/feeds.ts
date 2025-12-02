04c867d7d94ddea9bd93fb253186726a86a7a7fc
cutbypham <cutbypham@gmail.com>
2025-12-02 20:01:40 +0700

    👌Auto commit

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,3 +1,21 @@
 export const feeds = [
-    'https://geohot.github.io/blog/feed.xml',
+  'https://geohot.github.io/blog/feed.xml',
+  // Digital Spaghetti
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCTdsYrCpF2zRKWWL0SkaOtA',
+  // Natalie Lynn
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCkjrBN_GAjFJyVvjcI07KkQ',
+  // Viet Riders
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC_y92TyYNuw-QVk10i5IOAA',
+  // kodekai
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCuaGQ8RZR9JMu_AmzIRSyUw',
+  // Nomad Push
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCNIXUCzBMXoWu69iDOtbx5A',
+  // nor gather into barns
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCCIBKjzmbETY_tYG_BowaLA',
+  // 7789vlog
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UClK96yyeo-Hg3lZ86vrqVjQ',
+  // Mike Okay
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCpp3cHR9TWVyXqL1AVw4XkA',
+  // Đấy Chấm Than
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UClMkagB7TmoXeFJ_9YOv-Rw',
 ]
